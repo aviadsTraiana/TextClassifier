d32e84fb6b308cbc0803bc9a89547fce573b3457
aviadsTraiana <aviads@traiana.com>
2022-02-10 11:42:31 +0200

    testing WordTrie

---
+++ src/test/java/com/mcas/textclassifier/classifier/ds/WordTrieTest.java
@@ -0,0 +1,75 @@
+package com.mcas.textclassifier.classifier.ds;
+
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordTrieTest {
+
+    WordTrie trie;
+    @BeforeEach
+    void setUp() {
+        trie= new WordTrie();
+        trie.addWords(new String[]{"bank","account"},"financial");
+        trie.addWords(new String[]{"bank","account"},"fintech");
+        trie.addWords(new String[]{"bank","manager"},"financial");
+        trie.addWords(new String[]{"medical","insurance"},"healthcare");
+    }
+
+    @Test
+    void testHealthcareExist(){
+        trie.readNext("medical");
+        val insuranceNode = trie.readNext("insurance").map(i->i.getTags().stream().map(Tag::getValue).collect(Collectors.toSet()));
+        assertThat("insurance is not a child of medical after resetting", insuranceNode.isPresent());
+        assertThat(insuranceNode.get(), containsInAnyOrder("healthcare"));
+    }
+
+    @Test
+    void testNonExistingWord(){
+        val bankNode = trie.readNext("bank");
+        assertTrue(bankNode.isPresent());
+        assertFalse(trie.readNext("aviad").isPresent());
+    }
+
+    @Test
+    void testMultiTags() {
+        val bankNode = trie.readNext("bank");
+        assertTrue(bankNode.isPresent());
+        val accountNode = trie.readNext("account");
+        assertTrue(accountNode.isPresent());
+        assertTrue(accountNode.get().isTagged());
+        val tags = accountNode.get().getTags().stream().map(Tag::getValue).collect(Collectors.toSet());
+        assertThat(tags,containsInAnyOrder("financial","fintech"));
+    }
+
+    @Test
+    void isChildOfRoot() {
+        trie.readNext("bank");
+        assertTrue(trie.isChildOfRoot());
+        trie.readNext("account");
+        assertFalse(trie.isChildOfRoot());
+        trie.reset();
+        assertFalse(trie.isChildOfRoot());
+
+    }
+
+    @Test
+    void testNodeUniqueness(){
+        val bankNode1 = trie.readNext("bank");
+        assertTrue(trie.readNext("account").isPresent());
+        trie.reset();
+        val bankNode2 = trie.readNext("bank");
+        assertEquals(bankNode2, bankNode1);
+        val managerNode = trie.readNext("manager");
+        assertTrue(managerNode.isPresent());
+    }
+
+
+
+}
