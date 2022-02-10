80dcd404c8d35c8db3f27604ccb89bc388e21de4
aviadsTraiana <aviads@traiana.com>
2022-02-10 14:51:24 +0200

    separate app logic

--- src/main/java/com/mcas/textclassifier/App.java
+++ src/main/java/com/mcas/textclassifier/App.java
@@ -1,75 +1,28 @@
 package com.mcas.textclassifier;
 
-import com.mcas.textclassifier.classifier.Classifier;
-import com.mcas.textclassifier.configurations.data.ClassificationRules;
-import com.mcas.textclassifier.configurations.loaders.ClassificationRulesLoader;
-import com.mcas.textclassifier.configurations.loaders.JsonClassificationRulesLoader;
-import com.mcas.textclassifier.tokenizers.TokenizerConfiguration;
-import com.mcas.textclassifier.tokenizers.TokenizerStreamer;
-import com.mcas.textclassifier.types.Token;
-import com.mcas.textclassifier.types.TokenRange;
-import com.mcas.textclassifier.types.TokenType;
 import com.mcas.textclassifier.view.CommandLineView;
-import lombok.Cleanup;
 import lombok.val;
 
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.function.Predicate;
-
-import static com.mcas.textclassifier.utils.FileHelper.readFile;
 
 public class App {
-    private static final TokenizerConfiguration tokenConfig =
-            TokenizerConfiguration
-                    .builder()
-                    .lowerCaseMode(true)
-                    .wordRange(new TokenRange('a', 'z'))
-                    .build();
 
     public static void main(String[] args) {
         try {
             val cmd = new CommandLineView(args);
-            val config = loadConfiguration(Paths.get(cmd.getConfigPath()));
-            val classifier = new Classifier(config);
-            val files = Files.walk(Paths.get(cmd.getScanPath())).filter(Files::isRegularFile);
-            files.forEach(filePath ->{
-                try {
-                    classifyFile(classifier, filePath);
-                } catch (IOException e) {
-                    System.out.println("could not read file "+filePath+" skipping");
-                }
-            });
-        }catch (IllegalArgumentException e){
+            val textClassifier = new TextClassifierFilesReader(
+                    Paths.get(cmd.getConfigPath()),
+                    Paths.get(cmd.getScanPath())
+            );
+            textClassifier.run();
+        } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private static void classifyFile(Classifier classifier, Path filePath) throws IOException {
-        @Cleanup Reader reader = readFile(filePath);
-        val tokensStream =
-                new TokenizerStreamer(reader, tokenConfig)
-                        .stream()
-                        .filter(tokenFilterPredicates());
-        
-        classifier
-                .classifyTokens(tokensStream)
-                .forEach(System.out::println);
-    }
-
-    private static ClassificationRules loadConfiguration(Path path) throws IOException {
-        @Cleanup Reader reader = readFile(path);
-        ClassificationRulesLoader cl = new JsonClassificationRulesLoader();
-        return cl.load(reader);
-    }
-
-    private static Predicate<Token> tokenFilterPredicates() {
-        return t -> t.getType().equals(TokenType.WORD) || t.getType().equals(TokenType.NUMBER);
     }
 }
---
+++ src/main/java/com/mcas/textclassifier/TextClassifierFilesReader.java
@@ -0,0 +1,81 @@
+package com.mcas.textclassifier;
+
+import com.mcas.textclassifier.classifier.Classifier;
+import com.mcas.textclassifier.classifier.ds.Tag;
+import com.mcas.textclassifier.configurations.data.ClassificationRules;
+import com.mcas.textclassifier.configurations.loaders.ClassificationRulesLoader;
+import com.mcas.textclassifier.configurations.loaders.JsonClassificationRulesLoader;
+import com.mcas.textclassifier.tokenizers.TokenizerConfiguration;
+import com.mcas.textclassifier.tokenizers.TokenizerStreamer;
+import com.mcas.textclassifier.types.Token;
+import com.mcas.textclassifier.types.TokenRange;
+import com.mcas.textclassifier.types.TokenType;
+import lombok.Cleanup;
+import lombok.SneakyThrows;
+import lombok.val;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+import static com.mcas.textclassifier.utils.FileHelper.readFile;
+
+public class TextClassifierFilesReader implements Runnable {
+
+    final private Classifier classifier;
+    final private Path scanPath;
+
+    private static final TokenizerConfiguration tokenConfig =
+            TokenizerConfiguration
+                    .builder()
+                    .lowerCaseMode(true)
+                    .wordRange(new TokenRange('a', 'z'))
+                    .build();
+
+    public TextClassifierFilesReader(Path config, Path scan) throws IOException {
+        ClassificationRules classificationRules = loadConfiguration(config);
+        this.scanPath = scan;
+        this.classifier = new Classifier(classificationRules);
+    }
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        Files.walk(this.scanPath)
+                .filter(Files::isRegularFile)
+                .forEach(filePath -> {
+                    try {
+                        classifyFile(filePath);
+                    } catch (IOException e) {
+                        //todo: replace with logger
+                        System.err.println("could not read file " + filePath + " skipping");
+                    }
+                });
+    }
+
+    private void classifyFile(Path filePath) throws IOException {
+        @Cleanup Reader reader = readFile(filePath);
+        val tokensStream =
+                new TokenizerStreamer(reader, tokenConfig)
+                        .stream()
+                        .filter(tokenFilterPredicates());
+
+        this.classifier
+                .classifyTokens(tokensStream)
+                .stream()
+                .map(Tag::getValue)
+                .forEach(System.out::println);
+    }
+
+    private static ClassificationRules loadConfiguration(Path path) throws IOException {
+        @Cleanup Reader reader = readFile(path);
+        ClassificationRulesLoader cl = new JsonClassificationRulesLoader();
+        return cl.load(reader);
+    }
+
+    private static Predicate<Token> tokenFilterPredicates() {
+        return t -> t.getType().equals(TokenType.WORD) || t.getType().equals(TokenType.NUMBER);
+    }
+}
